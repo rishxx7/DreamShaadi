42c60627bcd638c451ba731ade58a8d8ea442c44
rishxx7 <rishabhsingh7781@gmail.com>
2026-05-27 00:47:28 +0530

    Fixed Vercel React routing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://https://dreamshaadi.onrender.com/api",
+  baseURL: "https://dreamshaadi.onrender.com/api",
 });
 
 export default API;
